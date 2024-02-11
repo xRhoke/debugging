492f0fc03a723ba026bf9ee28e58ee750c00a966
Elliott Hamilton <e2186519@rgcafc96405.lan>
2024-02-11 10:08:51 -0600

    initialize

---
+++ src/main/java/com/elliotthamilton/debugging/DebuggingApplication.java
@@ -0,0 +1,13 @@
+package com.elliotthamilton.debugging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebuggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebuggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elliotthamilton/debugging/DebuggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elliotthamilton.debugging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebuggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
